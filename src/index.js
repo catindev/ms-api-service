bd528bdaa9b395a33001db98246e37382f6bdb14
Владимир Тицкий <vladimir.titskiy@sms-online.com>
2017-09-21 00:40:24 +0600

    CORS

--- src/index.js
+++ src/index.js
@@ -10,11 +10,13 @@ Raven
 const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const app = express()
 
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
 
 app.use(require('./session'))
 app.use(require('./router'))
